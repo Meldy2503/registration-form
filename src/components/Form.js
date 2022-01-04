0ffc2c682d78d463ee32ce748ee4bc69ec8d3948
meldy2503 <emelder.charles25@gmail.com>
2022-01-03 23:50:07 -0800

    add form list

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,9 +12,14 @@ export default function Form() {
              confirmPassword: ""
          }
      })
+     console.log(formik.values)
     return (
-        <div>
-           <form action="">
+        <div className='container'>
+            <div className='container-sub1'>
+            <img className='image1' src="https://cdn.pixabay.com/photo/2020/09/17/18/05/people-5579977__340.png" alt="welcome-img" />
+            </div>
+            <div className='container-sub2'>
+           <form>
                <input 
                    type="text"
                    placeholder='first name'
@@ -22,6 +27,7 @@ export default function Form() {
                    value={formik.values.firstName}
                    onChange={formik.handleChange}
               />
+              <br />
                <input 
                    type="text"
                    placeholder='last name'
@@ -29,6 +35,7 @@ export default function Form() {
                    value={formik.values.lastName}
                    onChange={formik.handleChange}
                 />
+                <br />
                <input 
                    type="email"
                    placeholder='email'
@@ -36,6 +43,7 @@ export default function Form() {
                    value={formik.values.email}
                    onChange={formik.handleChange}
                 />
+                <br />
                 <input 
                    type="password"
                    placeholder='password'
@@ -43,6 +51,7 @@ export default function Form() {
                    value={formik.values.password}
                    onChange={formik.handleChange}
                 />
+                <br />
                 <input 
                    type="password"
                    placeholder='confirm password'
@@ -51,6 +60,7 @@ export default function Form() {
                    onChange={formik.handleChange}
                 />
            </form>
+           </div>
         </div>
     )
 }
